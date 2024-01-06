f725d41755fae89f717d10e1282005b356218e7c
Chris Rose <offline@offby1.net>
2024-01-06 14:36:27 -0800

    [ui] Nicer admin site titles

--- nominate/admin.py
+++ nominate/admin.py
@@ -146,3 +146,8 @@ admin.site.register(models.NominatingMemberProfile, NominatingMemberProfileAdmin
 admin.site.register(models.Category, CategoryAdmin)
 admin.site.register(models.Nomination, ExtendedNominationAdmin)
 admin.site.register(models.ReportRecipient, ReportRecipientAdmin)
+
+# Customize the Admin
+admin.site.site_title = "NomNom"
+admin.site.site_header = "NomNom Administration Interface"
+admin.site.index_title = "Hugo Administration"
