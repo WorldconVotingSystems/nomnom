8363ee75ff49027c88ee2ced8f80d13cda0ea320
Chris Rose <offline@offby1.net>
2024-02-10 08:01:00 -0800

    Add a management command to generate test nominator data

---
+++ nominate/management/commands/fake_nominator.py
@@ -0,0 +1,18 @@
+import djclick as click
+from model_bakery import baker
+from nominate.models import NominatingMemberProfile
+
+
+@click.command()
+@click.argument("nominator_id", nargs=-1)
+def command_name(nominator_id: list[str]):
+    nominator_ids = [int(n) for n in nominator_id]
+    existing_nominator_ids = NominatingMemberProfile.objects.filter(
+        id__in=nominator_ids
+    ).values_list("id", flat=True)
+
+    missing_id_list = [i for i in nominator_ids if i not in existing_nominator_ids]
+
+    for i in missing_id_list:
+        baker.make("nominate.NominatingMemberProfile", id=i)
+        print(f"Created profile {i}")
