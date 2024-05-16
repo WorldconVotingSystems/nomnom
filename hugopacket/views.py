db5a90940cb6f4a390260a2109ad595b1fb7e1ad
Chris Rose <offline@offby1.net>
2024-05-15 19:32:34 -0700

    Require that the member is logged in and has voting rights

--- hugopacket/views.py
+++ hugopacket/views.py
@@ -1,8 +1,14 @@
 from dataclasses import dataclass
 from datetime import datetime
+from functools import wraps
+from urllib.parse import urlparse
 
+from django.conf import settings
+from django.contrib.auth import REDIRECT_FIELD_NAME
+from django.contrib.auth.decorators import login_required
+from django.core.exceptions import PermissionDenied
 from django.http import Http404, HttpRequest, HttpResponse
-from django.shortcuts import get_object_or_404, redirect, render
+from django.shortcuts import get_object_or_404, redirect, render, resolve_url
 from django_svcs.apps import svcs_from
 from nominate.models import Election
 
@@ -29,6 +35,55 @@ class PacketFileDisplay:
         return self.packet_file.name
 
 
+def request_passes_test(
+    test_func, login_url=None, redirect_field_name=REDIRECT_FIELD_NAME
+):
+    """
+    Decorator for views that checks that the user passes the given test,
+    redirecting to the log-in page if necessary. The test should be a callable
+    that takes the user object and returns True if the user passes.
+    """
+
+    def decorator(view_func):
+        @wraps(view_func)
+        def _wrapper_view(request, *args, **kwargs):
+            if test_func(request):
+                return view_func(request, *args, **kwargs)
+            path = request.build_absolute_uri()
+            resolved_login_url = resolve_url(login_url or settings.LOGIN_URL)
+            # If the login url is the same scheme and net location then just
+            # use the path as the "next" url.
+            login_scheme, login_netloc = urlparse(resolved_login_url)[:2]
+            current_scheme, current_netloc = urlparse(path)[:2]
+            if (not login_scheme or login_scheme == current_scheme) and (
+                not login_netloc or login_netloc == current_netloc
+            ):
+                path = request.get_full_path()
+            from django.contrib.auth.views import redirect_to_login
+
+            return redirect_to_login(path, resolved_login_url, redirect_field_name)
+
+        return _wrapper_view
+
+    return decorator
+
+
+def member_can_vote():
+    def test_func(request: HttpRequest) -> bool:
+        election_id = request.resolver_match.kwargs.get("election_id")
+
+        if not get_object_or_404(Election, slug=election_id).user_can_vote(
+            request.user
+        ):
+            raise PermissionDenied()
+
+        return True
+
+    return request_passes_test(test_func)
+
+
+@login_required
+@member_can_vote()
 def index(request: HttpRequest, election_id: str) -> HttpResponse:
     election = get_object_or_404(Election, slug=election_id)
     packet = get_object_or_404(ElectionPacket, election=election)
@@ -70,6 +125,8 @@ def index(request: HttpRequest, election_id: str) -> HttpResponse:
     )
 
 
+@login_required
+@member_can_vote()
 def download_packet(
     request: HttpRequest, election_id: str, packet_file_id: int
 ) -> HttpResponse:
