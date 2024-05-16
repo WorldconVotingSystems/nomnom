7ffe7db189563833d1aba0e454bfceee1000d7bf
Chris Rose <offline@offby1.net>
2024-05-16 12:03:51 -0700

    Show a packet link in the election list

--- hugopacket/urls.py
+++ hugopacket/urls.py
@@ -5,7 +5,7 @@ from . import views
 app_name = "hugopacket"
 
 urlpatterns = [
-    path("<election_id>/", views.index),
+    path("<election_id>/", views.index, name="election_packet"),
     path(
         "<election_id>/<int:packet_file_id>/",
         views.download_packet,
