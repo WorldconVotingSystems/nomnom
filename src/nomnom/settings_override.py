2e8f38a26898651af93e39f07095c009634f79df
Chris Rose <offline@offby1.net>
2024-08-31 08:14:54 -0700

    lib-ify nomnom a bit more

---
+++ src/nomnom/settings_override.py
@@ -0,0 +1,5 @@
+# This file should be overwritten by container mounts, in order
+# to override complex configuration settings outside of the ones
+# managed in the environ-config in settings.py.
+
+# By default, this file is empty.
