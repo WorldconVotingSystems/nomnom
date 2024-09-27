fdbcfd6b849d473a8e4ee23de011690cf4493d02
Chris Rose <offline@offby1.net>
2024-09-27 07:58:49 -0700

    Ensure .env exists when starting CI

--- scripts/ci/db-up.sh
+++ scripts/ci/db-up.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 set -eux -o pipefail
 
+# .env must exist, but it can be empty
+touch .env
+
 docker compose -f docker-compose.yml up -d db redis
 counter=1
 while ! pg_isready -h $NOM_DB_HOST -p $NOM_DB_PORT -U $NOM_DB_USER; do
