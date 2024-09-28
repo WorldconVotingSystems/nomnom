4ac24ff6f17a60d1d54427c9f50a762726b0fc05
Chris Rose <offline@offby1.net>
2024-09-27 18:52:23 -0700

    Delete the unused settings from nommon's source

--- src/nomnom/settings_override.py
+++
@@ -1,5 +0,0 @@
-# This file should be overwritten by container mounts, in order
-# to override complex configuration settings outside of the ones
-# managed in the environ-config in settings.py.
-
-# By default, this file is empty.
