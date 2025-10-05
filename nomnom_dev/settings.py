3e65333ccd1f3cccfa1aaa3785d1a7fe87894079
Chris Rose <offline@offby1.net>
2025-10-05 08:34:35 -0400

    Convention customization story - self-hosting and easier templates
    - explicitly provide a convention_styles.html for override
    - [nomnom_dev] provide an empty main.css
    - Self-host fonts and bootstrap
    - remove the convention fonts configuration in favor of convention_styles.html
    - [template] add fontawesome and bootstrap static downloaders
    - [template] add django_google_fonts
    - look and feel: nomnom for the icon, not glasgow
    
    This incidentally also provides full support for offline development.
    
    This'll need a dep bump on lacon

--- nomnom_dev/settings.py
+++ nomnom_dev/settings.py
@@ -108,17 +108,20 @@ INSTALLED_APPS = [
     "logentry_admin",
     # Theming
     "django_bootstrap5",
-    "fontawesomefree",
+    "bootstrap",
+    "fontawesome",
+    # Fonts
+    "django_google_fonts",
     # HTMX support
     "django_htmx",
     # A healthcheck
     "watchman",
     # Markdown field support
     "markdownfield",
-    "nomnom.base",
     # the convention theme; this MUST come before the nominate app, so that its templates can
     # override the nominate ones.
     "nomnom_dev",
+    "nomnom.base",
     # The nominating and voting app
     "nomnom.nominate",
     "nomnom.canonicalize",
@@ -279,6 +282,8 @@ USE_X_FORWARDED_PORT = True
 
 STATIC_URL = "static/"
 STATIC_ROOT = cfg.static_file_root
+GOOGLE_FONTS_DIR = BASE_DIR / "nomnom_dev" / "static" / "google_fonts"
+STATICFILES_DIRS = [GOOGLE_FONTS_DIR]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
@@ -339,6 +344,12 @@ BOOTSTRAP5 = {
         "blank-safe": "nomnom.nominate.renderers.BlankSafeFieldRenderer",
     },
 }
+
+GOOGLE_FONTS = [
+    "Roboto",
+    "Roboto Slab",
+    "Gruppo",
+]
 
 # Email
 EMAIL_HOST = cfg.email.host
--- src/nomnom/tests/test_convention.py
+++ src/nomnom/tests/test_convention.py
@@ -16,21 +16,6 @@ def test_convention_theme_stylesheet_is_a_list():
     )
 
 
-def test_convention_theme_font_url_contains_the_font_url():
-    assert "fonts" in ConventionTheme(
-        stylesheets="css/nominate.css", font_urls="fonts"
-    ).get_font_urls(None)
-
-
-def test_convention_theme_font_url_is_a_list():
-    assert isinstance(
-        ConventionTheme(
-            stylesheets="css/nominate.css", font_urls="fonts"
-        ).get_font_urls(None),
-        list,
-    )
-
-
 def test_convention_theme_stylesheet_settings_is_a_list():
     assert {
         "url": "css/nominate.css",
@@ -49,19 +34,3 @@ def test_convention_theme_stylesheet_settings_when_remote():
     } in ConventionTheme(
         stylesheets="http://css/nominate.css", font_urls="fonts"
     ).get_stylesheet_settings(None)
-
-
-def test_convention_theme_font_url_settings_is_a_list():
-    assert ConventionTheme(
-        stylesheets="css/nominate.css", font_urls="fonts"
-    ).get_font_url_settings(None) == [
-        {"url": "fonts", "rel": "stylesheet", "static": True}
-    ]
-
-
-def test_convention_theme_font_url_settings_when_remote():
-    assert ConventionTheme(
-        stylesheets="css/nominate.css", font_urls="http://fonts"
-    ).get_font_url_settings(None) == [
-        {"url": "http://fonts", "rel": "stylesheet", "static": False}
-    ]
