e69d5f42c597de507ee4d34a709d52eaaa5647a8
Chris Rose <offline@offby1.net>
2026-03-09 17:13:59 -1000

    introduce structlog
    - add structlog to the dependencies
    - configure structlog in dev and production modes
    - configure structlog in the template (future use)
    - log as json in the dev server

--- src/nomnom/celery.py
+++ src/nomnom/celery.py
@@ -1,6 +1,8 @@
 import os
 
 from celery import Celery
+from celery.signals import setup_logging
+from django_structlog.celery.steps import DjangoStructLogInitStep
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "config.settings")
 
@@ -8,9 +10,23 @@ app = Celery("nomnom")
 
 app.config_from_object("django.conf:settings", namespace="CELERY")
 
+app.steps["worker"].add(DjangoStructLogInitStep)  # type: ignore[assignment]
+
 app.autodiscover_tasks()
 
 
 @app.task(bind=True, ignore_result=True)
 def debug_task(self):
     print(f"Request: {self.request!r}")
+
+
+@setup_logging.connect
+def setup_celery_logging(loglevel, logfile, format, colorize, **kwargs):
+    import logging
+
+    from django.conf import settings
+
+    # importing settings and using LOGGING should automatically
+    # configure structlog.
+    settings_log_config = settings.LOGGING
+    logging.config.dictConfig(settings_log_config)
