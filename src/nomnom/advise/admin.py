a9babe7c6ed358c5efd9b10515e7f174054d9fe8
Chris Rose <offline@offby1.net>
2026-03-29 22:29:15 -0700

    Replace the inline tabular view in advise with a table
    Fixes https://github.com/WorldconVotingSystems/nomnom/issues/172

--- src/nomnom/advise/admin.py
+++ src/nomnom/advise/admin.py
@@ -3,9 +3,9 @@ from typing import Any
 
 from django import forms
 from django.contrib import admin
-from django.db.models import QuerySet
 from django.http.request import HttpRequest
 from django.utils.html import format_html
+from django.utils.safestring import mark_safe
 
 from . import models
 
@@ -19,35 +19,13 @@ class ProposalForm(forms.ModelForm):
         fields = "__all__"
 
 
-class VoterTable(admin.TabularInline):
-    model = models.Vote
-    extra = 0
-    fields = ["member_name", "member_email", "vote_cast_at"]
-    readonly_fields = ["member_name", "member_email", "vote_cast_at"]
-
-    def get_queryset(self, request: HttpRequest) -> QuerySet[models.Vote]:
-        return super().get_queryset(request).select_related("membership")
-
-    def has_delete_permission(self, *args, **kwargs) -> bool:
-        return False
-
-    def member_name(self, instance: models.Vote) -> str:
-        return instance.membership.preferred_name
-
-    def member_email(self, instance: models.Vote) -> str:
-        return instance.membership.email
-
-    def vote_cast_at(self, instance: models.Vote) -> str:
-        return instance.created
-
-
 class ProposalAdmin(admin.ModelAdmin):
     model = models.Proposal
     form = ProposalForm
-    inlines = [VoterTable]
 
     list_display = ["name", "view_on_site_link"]
     search_fields = ["title", "description"]
+    readonly_fields = ["vote_list"]
 
     change_form_template = "admin/advise/proposal/change_form.html"
 
@@ -64,6 +42,13 @@ class ProposalAdmin(admin.ModelAdmin):
                 "fields": ["state"],
             },
         ),
+        (
+            "Votes",
+            {
+                "fields": ["vote_list"],
+                "description": "Votes are read-only and managed separately.",
+            },
+        ),
     ]
 
     def formfield_for_dbfield(self, db_field, request, **kwargs):
@@ -75,6 +60,21 @@ class ProposalAdmin(admin.ModelAdmin):
     def view_on_site_link(self, instance: models.Proposal) -> str:
         url = instance.get_absolute_url()
         return format_html('<a href="{}" target="_blank">View on site</a>', url)
+
+    def vote_list(self, obj):
+        if not obj.pk:
+            return "(save to see related objects)"
+
+        rows = []
+        for c in obj.vote_set.all():
+            rows.append(
+                f"<tr><td>{c.membership.preferred_name}</td><td>{c.membership.user.email}</td><td>{c.created}</tr>"
+            )
+
+        table_content = "\n".join(rows)
+        return mark_safe(
+            f"<table><tr><th>Name</th><th>Email</th><th>Cast</th></tr>{table_content}</table>"
+        )
 
 
 class VoteAdminDataAdmin(admin.StackedInline):
