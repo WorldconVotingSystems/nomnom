e9d0dea85e16e575a9877daca457da9744f772f9
Andrew January <andrewjanuary@gmail.com>
2026-04-28 16:42:06 +0100

    Render timeline vertically on small screens
    Rather than allowing the timeline to wrap, on small screens switch
    to a vertical view. In order to avoid taking up all the screen real
    estate in the vertical view, default to rendering it collapsed so
    only the "current" time is shown. This is the first transition from
    a completed to a non-completed event. The timeline can then be
    interacted with to toggle the non-collapsed view.

--- src/nomnom/timeline/receivers.py
+++ src/nomnom/timeline/receivers.py
@@ -10,10 +10,20 @@ def add_timeline_content(sender, request, **kwargs):
     if not switch_is_active(SWITCH_TIMELINE):
         return {}
     timeline_events = list(models.TimelineEvent.objects.all())
+    _annotate_collapsed_visibility(timeline_events)
     return {
         "timeline_events": timeline_events,
         "any_provisional": any(e.provisional for e in timeline_events),
     }
+
+
+def _annotate_collapsed_visibility(events):
+    boundary = next(
+        (i - 1 for i, e in enumerate(events) if not e.complete),
+        len(events) - 1,
+    )
+    for i, event in enumerate(events):
+        event.shown_collapsed = i in (boundary, boundary + 1, boundary + 2)
 
 
 index_content_load.connect(add_timeline_content)
